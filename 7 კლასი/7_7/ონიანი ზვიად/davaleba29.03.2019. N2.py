49ea25f7c28da8158995e7c20526ab054ecfba22
sc-199 <47024787+sc-199@users.noreply.github.com>
2019-03-28 21:15:23 +0400

    Add files via upload

---
+++ 7 კლასი/7_7/ონიანი ზვიად/davaleba29.03.2019. N2.py
@@ -0,0 +1,9 @@
+Python 3.7.1rc1 (v3.7.1rc1:2064bcf6ce, Sep 26 2018, 15:15:36) [MSC v.1914 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n = 0
+while n < 20:
+    n += 1
+    if n %2 == 0:
+        continue
+    else :
+        print (n
