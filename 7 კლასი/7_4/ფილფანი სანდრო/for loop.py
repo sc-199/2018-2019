834eee9cad0a3e14bfaaf2f8c10ac750dbe052e0
sc-199 <47024787+sc-199@users.noreply.github.com>
2019-04-04 00:01:56 +0400

    Add files via upload

---
+++ 7 კლასი/7_4/ფილფანი სანდრო/for loop.py
@@ -0,0 +1,9 @@
+print('samnishna kenti ricxvebis jami:')
+print('for ciklis gamoyenebit')
+s = 0
+i = 0
+for i in range(100):
+  i += 1
+  if i % 2 == 1:
+    s += i
+print('kent samnishna ricxvebis jamia __ ' + str(s))
