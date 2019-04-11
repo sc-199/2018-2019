8e4227696b50ddc730e121eaa1f90c24e2ae4193
sc-199 <47024787+sc-199@users.noreply.github.com>
2019-04-11 22:19:26 +0400

    Add files via upload

---
+++ 7 კლასი/7_7/მახნიაშვილი საბა/range.py
@@ -0,0 +1,12 @@
+n = int (input ("sheitanet ricxvi"))
+a = 7
+x = 0
+r = 0
+k = 0
+for k in range (0, (n // 7 - n // 14),1):
+    x += a
+    r += 1
+    print (a)
+    a += 14
+print ("am ricxvebis jamia", x)
+print ("sul asetia", r, "ricxvi")
