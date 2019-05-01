2d0bd304d16d6df12eb16a920f1ed0e2beb0927b
sc-199 <47024787+sc-199@users.noreply.github.com>
2019-05-01 19:30:52 +0300

    ლატარეა

---
+++ 7 კლასი/7_2/არქანია ლიზი/ლატარეა.py
@@ -0,0 +1,8 @@
+Python 3.3.2 (v3.3.2:d047928ae3f6, May 16 2013, 00:03:43) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> n = int(input("barieri:"))
+k = int(input("raodenoba:"))
+import random
+for i in range(k):
+  print(random.randint(1, n))
+print("Done!")
